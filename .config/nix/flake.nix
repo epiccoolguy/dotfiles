94cb6a93687b72fbc8e08ae465bf1574f2f780da
Miguel Lo-A-Foe <miguel@loafoe.dev>
2024-10-12 18:59:07 +0200

    nix fmt

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -20,144 +20,152 @@
     };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, homebrew-core, homebrew-cask, homebrew-bundle }:
-  let
-    configuration = { pkgs, config, ... }: {
-      nixpkgs.config.allowUnfree = true;
-
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages = with pkgs;
-        [
-          alacritty
-          awscli2
-          azure-cli
-          bat
-          fzf
-          git-credential-manager
-          google-cloud-sdk
-          mkalias
-          neovim
-          nixfmt-rfc-style
-          tmux
-          zoxide
-        ];
-
-      homebrew = {
-        enable = true;
-        taps = builtins.attrNames config.nix-homebrew.taps;
-        onActivation.cleanup = "zap";
-        onActivation.autoUpdate = true;
-        onActivation.upgrade = true;
-        brews = [
-          "mas"
-        ];
-        casks = [
-          "1password"
-          "firefox"
-          "google-chrome"
-          "iina"
-          "visual-studio-code"
-        ];
-        masApps = {
-          "WireGuard" = 1451685025;
-        };
-      };
-
-      fonts.packages = with pkgs;
-        [
-          (nerdfonts.override { fonts = [ "Monaspace" ]; })
-        ];
-
-
-      system.defaults = {
-        dock.autohide = true;
-      };
-
-      # START https://gist.github.com/elliottminns/211ef645ebd484eb9a5228570bb60ec3
-      system.activationScripts.applications.text = let
-        env = pkgs.buildEnv {
-          name = "system-applications";
-          paths = config.environment.systemPackages;
-          pathsToLink = "/Applications";
-        };
-      in
-        pkgs.lib.mkForce ''
-          # Set up applications.
-          echo "setting up /Applications..." >&2
-          rm -rf /Applications/Nix\ Apps
-          mkdir -p /Applications/Nix\ Apps
-          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-          while read src; do
-            app_name=$(basename "$src")
-            echo "copying $src" >&2
-            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-          done
-        '';
-      # END https://gist.github.com/elliottminns/211ef645ebd484eb9a5228570bb60ec3
-
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-
-      # Enable using Touch ID authentication for `sudo`
-      security.pam.enableSudoTouchIdAuth = true;
-    };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#macbook
-    darwinConfigurations.macbook = nix-darwin.lib.darwinSystem {
-      modules =
-      [
-        configuration
-        nix-homebrew.darwinModules.nix-homebrew
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+      nix-homebrew,
+      homebrew-core,
+      homebrew-cask,
+      homebrew-bundle,
+    }:
+    let
+      configuration =
+        { pkgs, config, ... }:
         {
-          nix-homebrew = {
-            # Install Homebrew under the default prefix
+          nixpkgs.config.allowUnfree = true;
+
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = with pkgs; [
+            alacritty
+            awscli2
+            azure-cli
+            bat
+            fzf
+            git-credential-manager
+            google-cloud-sdk
+            mkalias
+            neovim
+            nixfmt-rfc-style
+            tmux
+            zoxide
+          ];
+
+          homebrew = {
             enable = true;
-
-            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-            enableRosetta = false;
-
-            # User owning the Homebrew prefix
-            user = "miguel";
-
-            # Declarative tap management
-            taps = {
-              "homebrew/homebrew-core" = homebrew-core;
-              "homebrew/homebrew-cask" = homebrew-cask;
-              "homebrew/homebrew-bundle" = homebrew-bundle;
+            taps = builtins.attrNames config.nix-homebrew.taps;
+            onActivation.cleanup = "zap";
+            onActivation.autoUpdate = true;
+            onActivation.upgrade = true;
+            brews = [
+              "mas"
+            ];
+            casks = [
+              "1password"
+              "firefox"
+              "google-chrome"
+              "iina"
+              "visual-studio-code"
+            ];
+            masApps = {
+              "WireGuard" = 1451685025;
             };
-
-            # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
-            mutableTaps = false;
           };
-        }
-      ];
+
+          fonts.packages = with pkgs; [
+            (nerdfonts.override { fonts = [ "Monaspace" ]; })
+          ];
+
+          system.defaults = {
+            dock.autohide = true;
+          };
+
+          # START https://gist.github.com/elliottminns/211ef645ebd484eb9a5228570bb60ec3
+          system.activationScripts.applications.text =
+            let
+              env = pkgs.buildEnv {
+                name = "system-applications";
+                paths = config.environment.systemPackages;
+                pathsToLink = "/Applications";
+              };
+            in
+            pkgs.lib.mkForce ''
+              # Set up applications.
+              echo "setting up /Applications..." >&2
+              rm -rf /Applications/Nix\ Apps
+              mkdir -p /Applications/Nix\ Apps
+              find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+              while read src; do
+                app_name=$(basename "$src")
+                echo "copying $src" >&2
+                ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+              done
+            '';
+          # END https://gist.github.com/elliottminns/211ef645ebd484eb9a5228570bb60ec3
+
+          # Auto upgrade nix package and the daemon service.
+          services.nix-daemon.enable = true;
+          # nix.package = pkgs.nix;
+
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+
+          # Create /etc/zshrc that loads the nix-darwin environment.
+          programs.zsh.enable = true; # default shell on catalina
+          # programs.fish.enable = true;
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 5;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+
+          # Enable using Touch ID authentication for `sudo`
+          security.pam.enableSudoTouchIdAuth = true;
+        };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#macbook
+      darwinConfigurations.macbook = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              # Install Homebrew under the default prefix
+              enable = true;
+
+              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+              enableRosetta = false;
+
+              # User owning the Homebrew prefix
+              user = "miguel";
+
+              # Declarative tap management
+              taps = {
+                "homebrew/homebrew-core" = homebrew-core;
+                "homebrew/homebrew-cask" = homebrew-cask;
+                "homebrew/homebrew-bundle" = homebrew-bundle;
+              };
+
+              # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+              mutableTaps = false;
+            };
+          }
+        ];
+      };
+
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations.macbook.pkgs;
+
+      # Enable nixfmt as formatter
+      formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixfmt-rfc-style;
     };
-
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations.macbook.pkgs;
-
-    # Enable nixfmt as formatter
-    formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixfmt-rfc-style;
-  };
 }
