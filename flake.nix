0cd4944b01c84a6b6ac24b9868e6ac33278925b1
Miguel Lo-A-Foe <miguel@loafoe.dev>
2024-10-15 00:19:58 +0200

    update switch alias

--- flake.nix
+++ flake.nix
@@ -161,7 +161,7 @@
           programs.zsh = {
             enable = true;
             shellAliases = {
-              switch = "darwin-rebuild switch --flake ~/.config/nix";
+              switch = "darwin-rebuild switch --flake ~/.config/nix#macbook";
             };
           };
         };
